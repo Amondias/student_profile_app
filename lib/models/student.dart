800c09c7e38f5023db7b373cd4c18fbb74746386
Amondias <mbanangondojeanandre@gmail.com>
2026-05-31 12:20:32 +0000

    feat(model): add student model

---
+++ lib/models/student.dart
@@ -0,0 +1,13 @@
+class Student {
+  final String name;
+  final String email;
+  final String major;
+  final String level;
+
+  const Student({
+    required this.name,
+    required this.email,
+    required this.major,
+    required this.level,
+  });
+}
